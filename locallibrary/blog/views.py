41b6f441cb2f7e207d8073a26d6b8c4d3a07c96b
ch3coch3 <020611j87208x@gmail.com>
2020-08-23 23:21:50 +0800

    complete Create, Update, and Delete Posts

--- locallibrary/blog/views.py
+++ locallibrary/blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
+
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -12,34 +21,55 @@ posts  = [
         'other':'america'
     },
 ]
-
-
 def home(request):
     context = {
         'posts':Post.objects.all()
     }
     # return render(request, 'blog/home.html',context)
     return render(request, 'blog/mainPage.html',context)
-    # return HttpResponse('<doctype>...')
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/mainPage.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html',{'title':'about'})
     # return HttpResponse('<h1>Blog About</h1>')
 
-def register(request):
-    return render(request, 'blog/register.html')
-
-def Login(request):
-    return render(request, 'blog/Login.html')
-
-def share(request):
-    return render(request, 'blog/share.html')
-
-def forgetPass(request):
-    return render(request, 'blog/forgetPass.html')
-
-def mainPage(request):
-    context = {
-        'posts':Post.objects.all()
-    }
-    return render(request, 'blog/mainPage.html',context)
