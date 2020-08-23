c431a0b60e8d8a33d25e8f50eed2354d388926c9
ch3coch3 <020611j87208x@gmail.com>
2020-08-23 16:12:50 +0800

    begin user profile uploaded

--- locallibrary/users/forms.py
+++ locallibrary/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 gender_choice = [
     ('1','男'),
@@ -37,3 +38,15 @@ class UserRegisterForm(forms.ModelForm):
         user.set_password(user.password)
         user.save()
         return user
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        field = 'image'
--- locallibrary/users/views.py
+++ locallibrary/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -17,4 +17,12 @@ def register(request):
     # return render(request, 'users/register.html')
 @login_required
 def profile(request):
+    u_form = UserUpdateForm()
+    p_form = ProfileUpdateForm()
+
+    context = {
+        'u_form':u_form,
+        'p_form':p_form,
+        
+    }
     return render(request, 'users/profile.html')
