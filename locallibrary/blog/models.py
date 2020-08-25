24635cc6c3c64b5d5326e0562722ef0ea624b7e4
xinyixxx <xinyixx@alum.ccu.edu.tw>
2020-08-25 12:03:49 +0800

    上傳的按紐但沒辦法上傳

--- locallibrary/blog/models.py
+++ locallibrary/blog/models.py
@@ -3,13 +3,14 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     date_posted  = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     other = models.CharField(max_length=200)
-    content = RichTextField(blank=True,null=True)
+    content = RichTextUploadingField(blank=True,null=True)
     #content = models.TextField()
 
     def __str__(self):
--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,9 +19,12 @@ from django.urls import path, include
 from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
+from blog import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/',include('ckeditor_uploader.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/Login.html'), name='login'),
