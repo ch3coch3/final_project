cc7ec045db52b5ea13032ebaf4014e0d87bd2639
Bob <yyfscres@gmail.com>
2020-08-26 11:34:50 +0800

    修改profile

---
+++ locallibrary/blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','content')
+
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'author':forms.Select(attrs={'class':'form-control'}),
+            'other':forms.TextInput(attrs={'class':'form-control','placeholder':'America'}),
+            'content':forms.Textarea(attrs={'class':'form-control'}),
+        }
