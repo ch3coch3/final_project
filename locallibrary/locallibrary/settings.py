de6b746ce2e060c2b458bfed8f22b7880fe40475
ch3coch3 <020611j87208x@gmail.com>
2020-10-10 15:01:47 +0800

    first initial

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -88,8 +88,8 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myData',
     }
 }
 
